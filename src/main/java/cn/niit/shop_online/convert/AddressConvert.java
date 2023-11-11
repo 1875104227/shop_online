62846690503a4a0837b204458cf9fb61b148a65b
“yaaii” <18751074229@163.com>
2023-11-11 19:13:45 +0800

    添加地址和修改地址

---
+++ src/main/java/cn/niit/shop_online/convert/AddressConvert.java
@@ -0,0 +1,26 @@
+package cn.niit.shop_online.convert;
+
+import cn.niit.shop_online.entity.UserShippingAddress;
+import cn.niit.shop_online.vo.AddressVO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+/**
+ * @author 86187
+ * @Date 2023/11/11
+ */
+@Mapper
+public interface AddressConvert {
+    AddressConvert INSTANCE = Mappers.getMapper(AddressConvert.class);
+
+
+    UserShippingAddress convert(AddressVO addressVO);
+
+
+    List<AddressVO> convertToAddressVOList(List<UserShippingAddress> addressList);
+
+
+    AddressVO convertToAddressVO(UserShippingAddress userShippingAddress);
+}
--- src/main/java/cn/niit/shop_online/service/UserShippingAddressService.java
+++ src/main/java/cn/niit/shop_online/service/UserShippingAddressService.java
@@ -1,7 +1,10 @@
 package cn.niit.shop_online.service;
 
 import cn.niit.shop_online.entity.UserShippingAddress;
+import cn.niit.shop_online.vo.AddressVO;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +15,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2023-11-07
  */
 public interface UserShippingAddressService extends IService<UserShippingAddress> {
-
+    Integer saveShippingAddress(AddressVO addressVO);
+    Integer editShippingAddress(AddressVO addressVO);
+    List<AddressVO> getList(Integer userId);
 }
