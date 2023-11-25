4c8e15742e1289b19bde32f27d565576b551f323
“yaaii” <18751074229@163.com>
2023-11-25 13:30:08 +0800

    填写订单 - 获取再次购买订单

--- src/main/java/cn/niit/shop_online/convert/UserOrderDetailConvert.java
+++ src/main/java/cn/niit/shop_online/convert/UserOrderDetailConvert.java
@@ -2,12 +2,14 @@ package cn.niit.shop_online.convert;
 
 import cn.niit.shop_online.entity.UserOrder;
 import cn.niit.shop_online.vo.OrderDetailVO;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 /**
  * @author 86187
  * @Date 2023/11/25
  */
+@Mapper
 public interface UserOrderDetailConvert {
     UserOrderDetailConvert INSTANCE= Mappers.getMapper(UserOrderDetailConvert.class);
 
